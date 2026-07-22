ce57d98691b9cd9a30130ec6ae525de88307e79e
Yair Shachar <yairshachar@gmail.com>
2026-07-22 10:54:24 +0200

    Update dont-forget-your-breaks to v1.8.6

--- Casks/dont-forget-your-breaks.rb
+++ Casks/dont-forget-your-breaks.rb
@@ -1,6 +1,6 @@
 cask "dont-forget-your-breaks" do
-  version "1.8.5"
-  sha256 "53496687c795d7ff9cb41d9fb6b7235bc0ab363f94947a2c340dde81a648be46"
+  version "1.8.6"
+  sha256 "6e82a6d0bcd9d3d57f5c8b9d4cf22d8a7bff8a58b11066c0e5666078f608970d"
 
   url "https://github.com/YairShachar/dont-forget-your-breaks/releases/download/v#{version}/DontForgetYourBreaks.dmg"
   name "Don't Forget Your Breaks"
